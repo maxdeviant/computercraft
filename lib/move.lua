20da056182181440e1a9bc860a96411d20da076d
Marshall Bowers <git@maxdeviant.com>
2025-07-05 00:15:03 -0400

    Factor out `traverse_plane` function from shaft miner

--- lib/move.lua
+++ lib/move.lua
@@ -36,4 +36,35 @@ function move.down(n)
     end
 end
 
+--- Traverses a plane with the specified width and height, invoking the provided action at each block.
+---
+--- @param width number
+--- @param height number
+--- @param action fun(): nil
+function move.traverse_plane(width, height, action)
+    for x = 1,width do
+        for _ = 1,height - 1 do
+            action()
+            turtle.forward()
+        end
+
+        if x < width then
+            if x % 2 == 0 then
+                turtle.turnLeft()
+                action()
+                turtle.forward()
+                turtle.turnLeft()
+            else
+                turtle.turnRight()
+                action()
+                turtle.forward()
+                turtle.turnRight()
+            end
+        end
+    end
+
+    turtle.turnRight()
+    turtle.turnRight()
+end
+
 return move
--- shaft_miner.lua
+++ shaft_miner.lua
@@ -1,34 +1,10 @@
-local function mine_shaft_layer(width, height)
-    for x = 1,width do
-        for _ = 1,height - 1 do
-            turtle.dig()
-            turtle.forward()
-        end
-
-        if x < width then
-            if x % 2 == 0 then
-                turtle.turnLeft()
-                turtle.dig()
-                turtle.forward()
-                turtle.turnLeft()
-            else
-                turtle.turnRight()
-                turtle.dig()
-                turtle.forward()
-                turtle.turnRight()
-            end
-        end
-    end
-
-    turtle.turnRight()
-    turtle.turnRight()
-end
+local move = require "lib.move"
 
 local function mine_shaft(depth, size)
     for _ = 1,depth do
         turtle.digDown()
         turtle.down()
-        mine_shaft_layer(size, size)
+        move.traverse_plane(size, size, turtle.dig)
     end
 end
 
