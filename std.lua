66b34a5768d6ce3d71b03b1ccf22f577ca8baae0
Marshall Bowers <git@maxdeviant.com>
2025-07-06 11:31:09 -0400

    Add `std.is_main`

--- std.lua
+++ std.lua
@@ -1,5 +1,13 @@
 local std = {}
 
+--- Returns whether the current script is running as the main script.
+---
+--- @return boolean
+function std.is_main()
+    -- We're adding an additional frame to account for the `std` module being loaded.
+    return not pcall(debug.getlocal, 4 + 1, 1)
+end
+
 --- Repeats the given function *n* times.
 ---
 --- @param n number
