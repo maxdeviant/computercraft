d95cf3435510201c02411b46b45a614e4637e772
Marshall Bowers <git@maxdeviant.com>
2025-07-07 19:07:54 -0400

    shaft_miner: Allow using separate values for width and height

--- crates/computercraft_tests/src/tests/shaft_miner_tests.rs
+++ crates/computercraft_tests/src/tests/shaft_miner_tests.rs
@@ -24,3 +24,14 @@ fn test_shaft_miner() {
         .unwrap();
     assert_eq!(simulator.turtle().position, Position::new(0, -4, -0));
 }
+
+#[test]
+fn test_shaft_miner_with_different_width_and_height_values() {
+    let mut simulator = Simulator::new().unwrap();
+    set_script_root(&mut simulator);
+
+    simulator
+        .call_lua_file::<_, ()>("programs/shaft_miner.lua", (1, 3, 5))
+        .unwrap();
+    assert_eq!(simulator.turtle().position, Position::new(2, -1, -4));
+}
--- programs/shaft_miner.lua
+++ programs/shaft_miner.lua
@@ -1,23 +1,24 @@
 local move = require("lib.move")
 
-local function mine_shaft(depth, size)
+local function mine_shaft(depth, width, height)
     for _ = 1, depth do
         turtle.digDown()
         turtle.down()
-        move.traverse_plane(size, size, turtle.dig)
+        move.traverse_plane(width, height, turtle.dig)
     end
 end
 
 local function main(...)
     local args = { ... }
     local depth = args[1]
-    local size = args[2]
+    local width = args[2]
+    local height = args[3] or width
 
     print("Starting shaft miner")
     print("Depth: " .. depth)
-    print("Size: " .. size .. "x" .. size)
+    print("Size: " .. width .. "x" .. height)
 
-    mine_shaft(tonumber(depth), tonumber(size))
+    mine_shaft(tonumber(depth), tonumber(width), tonumber(height))
 end
 
 main(...)
